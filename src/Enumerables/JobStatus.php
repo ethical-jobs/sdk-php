9a13492607ca32d1ff5a66ac37292d5f352b6529
nikunj <nikunj@nikunj.info>
2021-09-15 10:16:24 +1000

    add comment about renaming constants

--- src/Enumerables/JobStatus.php
+++ src/Enumerables/JobStatus.php
@@ -7,6 +7,11 @@ namespace EthicalJobs\SDK\Enumerables;
  */
 class JobStatus extends Enum
 {
+    /**
+     * The Enum base class has a __callStatic method defined which returns the const variable name if invoked.
+     * So renaming the variable to get consistent value when using constant directly
+     *
+     */
     const DRAFT = "DRAFT";
 
     const PENDING = "PENDING";
